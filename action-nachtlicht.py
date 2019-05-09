f3d7792a1de998c9c2a4957cc729ae6281559e81
tierlord <xtra-j@web.de>
2019-05-10 00:14:59 +0200

    Update action-nachtlicht.py

--- action-nachtlicht.py
+++ action-nachtlicht.py
@@ -4,58 +4,6 @@ import paho.mqtt.client as mqtt
 
 MQTT_ADDR = "localhost"
 
-# def set_led(color):
-#     led.set_pixel(0, color[0], color[1], color[2])
-#     led.show()
-
-# def fade_in():
-#     time.sleep(1)
-#     r = 100
-#     g = 75
-#     b = 55
-#     while r<255:
-#         set_led([r,g,b])
-#         r += 1
-#         g += 1
-#         b += 1
-#         time.sleep(0.02)
-
-
-# def fade_slow(mins):
-#     time.sleep(2)
-#     fade_in()
-#     start_time = time.time()
-#     secs = mins * 60
-#     r = 255
-#     g = 230
-#     b = 210
-#     while r>0:
-#         set_led([r,g,b])
-#         if time.time() - start_time < (secs - 30):
-#             time.sleep(1)
-#         else:
-#             r -= 1
-#             if g > 0:
-#                 g -= 1
-#             if b > 0:
-#                 b -= 1
-#             time.sleep(0.115)
-
-# def fade_fast():
-#     time.sleep(0.5)
-#     r = 255
-#     g = 230
-#     b = 210
-#     while r>0:
-#         set_led([r,g,b])
-#         if r > 0:
-#             r -= 1
-#         if g > 0:
-#             g -= 1
-#         if b > 0:
-#             b -= 1
-#         time.sleep(0.04)
-
 def nachtlicht_callback(hermes, message):
     request = 0
     anAus = "an"
